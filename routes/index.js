7c0ca2dc3d3effe3543074430cac93b884d174b1
Urb4nOutl4w <45825633+Urb4nOutl4w@users.noreply.github.com>
2021-11-12 13:19:21 +0100

    dasd
    asdasd

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express sdfdsfsd' });
+  res.render('index', { title: 'Express Hallo Welt Juhuuuuuuu' });
 });
 
 module.exports = router;
