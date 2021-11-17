54332b81339edeb985afe369b5c94d1923a0cf52
Urb4nOutl4w <45825633+Urb4nOutl4w@users.noreply.github.com>
2021-11-17 13:27:49 +0100

    Update index.js
    Test Heute

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Hallo Welt Juhuuuuuuu' });
+  res.render('index', { title: 'Express Hallo Welt Juhuuuuuuu 13:27 17.11.' });
 });
 
 module.exports = router;
